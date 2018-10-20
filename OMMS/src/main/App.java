d7e655f4c59ee1c1ecd790fe93fac579fcf45ffa
JohnFromMars <rock13810@gmail.com>
2018-10-20 09:20:11 +0800

    app complete

--- OMMS/src/main/App.java
+++ OMMS/src/main/App.java
@@ -8,11 +8,11 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello to OMMS main function");
-		
+		System.out.println("Welcome to OMMS main function");
 		System.out.println("Visitor functions :" + new VisitorService().getServiceName());
 		System.out.println("Member functions :" + new MemberService().getServiceName());
 		System.out.println("Admin functions :" + new AdminService().getServiceName());
+		System.out.println("End...");
 
 	}
 
