065e4f4341ee42a890659e9077394142564eb24f
JohnFromMars <rock13810@gmail.com>
2018-10-20 09:49:44 +0800

    member service function A

--- OMMS/src/services/MemberService.java
+++ OMMS/src/services/MemberService.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class MemberService {
 	public List<String> getServiceName() {
 		List<String> serviceNames = new ArrayList<>();
+		serviceNames.add("Member Function A");
 		serviceNames.add("Member Function B");
 		serviceNames.add("Member Function C");
 		return serviceNames;
