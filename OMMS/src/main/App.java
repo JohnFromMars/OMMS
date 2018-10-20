dea41aca93d9a98735f1b24e6f35a6135e3f3c6b
JohnFromMars <rock13810@gmail.com>
2018-10-20 09:01:39 +0800

    Member service complete

--- OMMS/src/main/App.java
+++ OMMS/src/main/App.java
@@ -1,10 +1,16 @@
 package main;
 
+import services.MemberService;
+import services.VisitorService;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello to OMMS main");
+		System.out.println("Hello to OMMS main function");
+		
+		System.out.println("Visitor functions :" + new VisitorService().getServiceName());
+		System.out.println("Visitor functions :" + new MemberService().getServiceName());
 
 	}
 
---
+++ OMMS/src/services/MemberService.java
@@ -0,0 +1,15 @@
+package services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberService {
+	public List<String> getServiceName() {
+		List<String> serviceNames = new ArrayList<>();
+		serviceNames.add("Member Function A");
+		serviceNames.add("Member Function B");
+		serviceNames.add("Member Function C");
+		return serviceNames;
+	}
+
+}
